f29dd1f265ea8b1b82f9d7dcbd1ef324620a606f
Adam Hawkins <hi@ahawkins.me>
2018-08-09 23:39:12 +0700

    fix(rubocop): add missing title

--- test/pipeline_test.rb
+++ test/pipeline_test.rb
@@ -2,14 +2,17 @@
 
 require 'minitest/autorun'
 require 'yaml'
+require 'json'
 
 class PipelineTest < MiniTest::Test
   PIPELINE_FILE = '.buildkite/pipeline.yml'
+  TITLES_FILE =  'titles.json'
 
-  attr_reader :pipeline
+  attr_reader :pipeline, :titles
 
   def setup
     @pipeline = YAML.safe_load(File.new(PIPELINE_FILE))
+    @titles = JSON.load(File.new(TITLES_FILE))
   end
 
   def test_timeout
@@ -32,6 +35,13 @@ class PipelineTest < MiniTest::Test
       end
 
       assert step, "No step for: #{script}"
+    end
+  end
+
+  def test_title_for_pipeline_steps
+    pipeline.fetch('steps').each do |entry|
+      name = entry.fetch('label')
+      assert titles.dig(name), "#{name} missing title entry"
     end
   end
 end
